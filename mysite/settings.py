772e62889c06ecc229729664004a6c6db4e9e248
Mahmoud-Harb <modafox2@yahoo.com>
2018-08-10 20:30:16 +0200

    Production project-2

--- mysite/settings.py
+++ mysite/settings.py
@@ -67,12 +67,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite_db',
-        'PASSWORD': 'forex4living2b',
-        'HOST': 'localhost',
-        'port': '3306',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
